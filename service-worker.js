d1773a2394fd8328b0d944f18259e458b7aa608c
NghiTuanLoc <140692163+nghituanloc@users.noreply.github.com>
2025-12-12 16:52:38 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'xsmn-cache-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  // Network-first cho dữ liệu động (API), cache-first cho tĩnh
+  if (request.url.includes('minhngoc.com.vn/getkqxs')) {
+    event.respondWith(
+      fetch(request).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, resClone));
+        return res;
+      }).catch(() => caches.match(request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(request).then((cached) => cached || fetch(request))
+    );
+  }
+});
+
+
